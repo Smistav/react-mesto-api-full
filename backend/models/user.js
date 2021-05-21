94edd217c71c06dc4f6790105e5432054a1d60a0
Stas Smirnov <smistav@ya.ru>
2021-05-21 08:23:22 +0300

    fix regular string test AVA

--- backend/models/user.js
+++ backend/models/user.js
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     validate: {
       validator(ava) {
-        return /https?:\/\/(www\.)?[a-z0-9-.]{3,20}[a-z]{2,6}[-._~:/[\]?#@!$&'()*+,;=a-z0-9]*/g.test(ava);
+        return /^https?:\/\/(www{3}\.)?[a-z0-9-]{3,20}\.[a-z]{2,6}[-._~:/[\]?#@!$&'()*+,;=a-z0-9]*/g.test(ava);
       },
     },
     required: true,
